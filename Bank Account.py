855c4cbff51cd623e873511ce6f69d7efaef9951
Suryansh Singh Solanki <suryanshsolanki9@gmail.com>
2026-09-14 23:44:23 +0530

    edit

--- Bank Account.py
+++ Bank Account.py
@@ -8,15 +8,18 @@ class BankAccount:
         print(f"{amount} deposited, your current balance is {self.balance}")
 
     def withdraw(self, amount):
-        self.balance -= amount
-        print(f"{amount} withdrawn, your current balance is {self.balance}")
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"{amount} withdrawn, your current balance is {self.balance}")
+        else:
+            print("Insufficient balance")
 
     def check_balance(self):
         print(f"Current balance: {self.balance}")
 
-owner1 = BankAccount("Surya", 20000)
-owner2 = BankAccount("Dhiraj", 150000)
+account1 = BankAccount("Surya", 20000)
+account2 = BankAccount("Dhiraj", 150000)
 
-owner1.deposit(15000)
-owner1.withdraw(6000)
-owner1.check_balance()
+account1.deposit(15000)
+account1.withdraw(6000)
+account1.check_balance()
