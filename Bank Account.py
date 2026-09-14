2220bc749d6a3e8b406cfa069707541e2ea4e18a
Suryansh Singh Solanki <suryanshsolanki9@gmail.com>
2026-09-14 23:29:04 +0530

    object created

---
+++ Bank Account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def check_balance(self):
+        pass
+
+owner1 = BankAccount("Surya", 20000)
+owner2 = BankAccount("Dhiraj", 150000)
